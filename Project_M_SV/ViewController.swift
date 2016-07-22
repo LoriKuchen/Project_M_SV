c68a9f3fe18b61dc7a93ddc093ed0531a17eff4c
Lori Kuchen <lorikuchen@loris-macbook-pro.local>
2016-07-22 11:15:36 +0200

    Initial Commit

---
+++ Project_M_SV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project_M_SV
+//
+//  Created by Lori Kuchen on 22.07.16.
+//  Copyright © 2016 Lori Kuchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
